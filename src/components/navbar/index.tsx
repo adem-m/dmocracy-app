6a4a509c9930958c73d995b8d9e3f8518f7f21be
Paolo Manaois <paolo.manaois@yahoo.fr>
2022-10-25 18:06:50 +0200

    css fixes

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,21 +1,142 @@
+import * as React from 'react';
 import { NavLink } from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
 import styles from "./navbar.module.scss";
 
-function Navbar() {
-  return (
-    <nav className={styles.navbar}>
-      <NavLink 
-        className={({ isActive }) => isActive ? styles.navLinkActive : "" }
-        to={"/"} end>
-          Home
-      </NavLink>
-      <NavLink 
-        className={({ isActive }) => isActive ? styles.navLinkActive : "" }
-        to={"/voting-sessions"}>
-          Voting Sessions
-      </NavLink>
-    </nav>
-  )
+const pages = {
+  "Home": "/",
+  "Voting Sessions": "/voting-sessions"
 }
 
-export default Navbar;
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const [isConnected, setIsConnected] = React.useState(false);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            DMOCRACY
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              className={styles.mobileMenu}
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {Object.entries(pages).map(([key, value]: [string, string]) => (
+                <MenuItem key={key} onClick={handleCloseNavMenu}>
+                  <NavLink 
+                    className={({ isActive }) => isActive ? styles.navLinkActive : styles.navLink }
+                    to={value} end>
+                      {key}
+                  </NavLink>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            DMOCRACY
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {Object.entries(pages).map(([key, value]: [string, string]) => (
+              <NavLink 
+                className={({ isActive }) => isActive ? styles.navLinkActive : styles.navLink }
+                to={value} end>
+                {key}
+              </NavLink>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              { isConnected 
+                ? <IconButton sx={{ p: 0 }} onClick={_ => setIsConnected(false)}>
+                    <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                  </IconButton>
+                : <button className={styles.connectionBtn} onClick={_ => setIsConnected(true)}>Connect Wallet</button>
+              }
+            </Tooltip>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default ResponsiveAppBar;
