2fc35b48d043294ccdc29cc8f380ef5f6d4374a2
Adem Mrizak <adem.mrizak@gmail.com>
2022-12-14 17:12:47 +0100

    My sessions removed

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,27 +1,23 @@
 import { lazy, Suspense } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import { useAccount } from "wagmi";
 
 const Home = lazy(() => import("./pages/Home"));
 const VotingSessions = lazy(() => import("./pages/VotingSessions"));
 const VotingSession = lazy(() => import("./pages/VotingSession"));
 const NewVotingSession = lazy(() => import("./pages/NewVotingSession"));
-const MyVotingSessions = lazy(() => import("./pages/MyVotingSessions"));
 
 function AppRouter() {
-  const { address, isConnected } = useAccount();
-
   return (
     <Suspense>
       <Routes>
-        <Route 
-          key={"home"} 
-          path={"/"} 
+        <Route
+          key={"home"}
+          path={"/"}
           element={<Home/>}/>
 
-        <Route 
-          key={"votingSessions"} 
-          path={"/voting-sessions"} 
+        <Route
+          key={"votingSessions"}
+          path={"/voting-sessions"}
           element={<VotingSessions/>}/>
 
         <Route
@@ -35,16 +31,6 @@ function AppRouter() {
           path={"/new-voting-session"}
           element={<NewVotingSession/>}
         />
-
-        {(!isConnected || address === undefined)
-          ? <></>  
-          : (<Route
-              key={"myVotingSessions"}
-              path={"/my-voting-sessions"}
-              element={<MyVotingSessions/>}
-            />
-          )
-        }
 
         <Route
           key={"default"}
--- src/rules/nav-rules.ts
+++ src/rules/nav-rules.ts
@@ -2,7 +2,6 @@ const NAVRULES: Record<string, { path: string, isWalletRequired: boolean }> = {
   "Home": { path: "/", isWalletRequired: false },
   "Voting Sessions": { path: "/voting-sessions", isWalletRequired: false },
   "Create Voting Session": { path: "/new-voting-session", isWalletRequired: false },
-  "My Sessions": { path: "/my-voting-sessions", isWalletRequired: true },
 }
 
 export default NAVRULES;
